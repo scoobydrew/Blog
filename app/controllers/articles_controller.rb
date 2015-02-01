5ba7f472b53b73359548e92a10cf5bec192a9955
scoobydrew <drewcsmith3@gmail.com>
2015-01-31 19:55:52 -0600

    devise views and login modal

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "drewsmith", password: "secret", except: [:index, :show]
+  
   def new
     @article = Article.new
   end
