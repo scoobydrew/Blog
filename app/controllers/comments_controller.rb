c72888e776738646f413632a6955b0640254bb80
scoobydrew <drewcsmith3@gmail.com>
2015-05-08 10:39:06 -0500

    Commenting restricted to users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,22 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "drewsmith", password: "secret", only: :destroy
+  before_filter :authenticate_user!
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
-  
+
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
+    if current_user.role == admin then
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+    end
     redirect_to article_path(@article)
   end
-  
+
   private
+
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
